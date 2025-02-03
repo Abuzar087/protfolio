9b456dbfebd97626efb7b282360d847549d45a80
Abuzar087 <hajadsaleem.88@gmail.com>
2025-02-04 00:29:13 +0500

    change i made

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Start build
 echo "BUILD START"
 
-# Install dependencies from requirements.txt
-python3 -m pip install --upgrade pip
+# Install dependencies
 python3 -m pip install -r requirements.txt
 
 # Collect static files
 python3 manage.py collectstatic --noinput --clear
 
+# End build
 echo "BUILD END"
