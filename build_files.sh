ccb02b8d058f458f5dda1e73cba2c30efb237491
Abuzar087 <hajadsaleem.88@gmail.com>
2025-02-04 00:01:19 +0500

    cha

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+# Start build
 echo "BUILD START"
+
+# Install dependencies
 python -m pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput --clear
+
+# End build
 echo "BUILD END"
