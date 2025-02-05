96d7e51248a2b3917b0522fc0ea5cc1468610d66
Abuzar087 <hajadsaleem.88@gmail.com>
2025-02-06 00:46:13 +0500

    noww

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,10 @@
 echo "BUILD START"
 
 # Install dependencies
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 # End build
 echo "BUILD END"
