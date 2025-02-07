0f679ecff10f5735bb292495d3d4297c76ceba1a
Abuzar087 <hajadsaleem.88@gmail.com>
2025-02-07 21:19:26 +0500

    changig after

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
-#from home.models import Home
-from home.models import Contact
+# from django.contrib import admin
+# #from home.models import Home
+# from home.models import Contact
 
-# Register your models here.
+# # Register your models here.
 
-#admin.site.register(Home)
-admin.site.register(Contact)
+# #admin.site.register(Home)
+# admin.site.register(Contact)
